9c9171d754c9c1c38e4d516db8b9c72764e9e1c5
Karent Narvaez <karent.narvaez@asurion.com>
2019-12-13 21:18:46 -0500

    creating models

--- ticket_office/models.py
+++ ticket_office/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+
+class Room(models.Model):
+    name = models.CharField(max_length=50)
+    capacity = models.IntegerField(default=0)
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=200)
+
+
+class Showtime(models.Model):
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    time = models.TimeField()
+
+
+class Customer(models.Model):
+    name = models.CharField(max_length=200)
+
+
+class Ticket(models.Model):
+    showtime = models.ForeignKey(Showtime, on_delete=models.CASCADE)
+    client = models.ForeignKey(Customer, on_delete=models.CASCADE)
+
+
+
+
---
+++ ticket_office/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+
+from ticket_office import views
+
+urlpatterns = [
+    # path('', views.index, name='index'),
+]
